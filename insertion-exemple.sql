a3c3b7a2a74ff663b4f35ea0d3085a159ff1df15
Yanis Oulhaci <yaniscrab@gmail.com>
2022-10-18 00:39:39 +0100

    add sql file for database structure

---
+++ insertion-exemple.sql
@@ -0,0 +1,17 @@
+INSERT INTO users VALUE(default, 'Salah06', "Salah", "Iboudghacene", "salahiboudghacene@gmail.com", "$2b$10$jvrFOpC.bpJvJrS6FfzCL.KGXGy18CN1LLltTQ4R5GwN0q3N9oC1i", "https://i.pinimg.com/280x280_RS/9b/96/da/9b96da51d64e39eec46f943f1e810006.jpg", "1998/07/12", default, 1, default, default, "2022-05-15 23:37:47", "2022-05-15 23:37:47");
+
+INSERT INTO users VALUE(default, 'Sidali06', "Sidali", "Mouhous", "sidalimouhous09@gmail.com", "$2b$10$jvrFOpC.bpJvJrS6FfzCL.KGXGy18CN1LLltTQ4R5GwN0q3N9oC1i", "http://image.noelshack.com/fichiers/2022/20/2/1652755578-13403874-631364133688659-4985272336506867684-o.jpeg", "2000/10/25", 1, 1, default, default, "2022-05-15 23:37:47", "2022-05-15 23:37:47");
+
+INSERT INTO users VALUE(default, 'Hamza2000', "Mohamed Hamza", "Mezadi", "mezadimohamedhamza@gmail.com", "$2b$10$jvrFOpC.bpJvJrS6FfzCL.KGXGy18CN1LLltTQ4R5GwN0q3N9oC1i", "https://yt3.ggpht.com/ytc/AKedOLSI_d5WQIu1Hhsh21pkJU4k7xIqzPtRs7x8n7KztZ8=s900-c-k-c0x00ffffff-no-rj", "2000/05/09", 2, 1, default, default, "2022-05-15 23:37:47", "2022-05-15 23:37:47");
+
+INSERT INTO users VALUE(default, 'Yanis02015', "Yanis", "Oulhaci", "yaniscrab@gmail.com", "$2b$10$jvrFOpC.bpJvJrS6FfzCL.KGXGy18CN1LLltTQ4R5GwN0q3N9oC1i", "https://avatars.githubusercontent.com/u/8948640?v=4", "1998/03/07", 3, 1, default, default, "2022-05-15 23:37:47", "2022-05-15 23:37:47");
+
+INSERT INTO users VALUE(default, 'Mouloud06', "Mouloud", "MSD", "mouloud@gmail.com", "$2b$10$jvrFOpC.bpJvJrS6FfzCL.KGXGy18CN1LLltTQ4R5GwN0q3N9oC1i", "default", "2000/01/01", default, 1, default, default, "2022-05-15 23:37:47", "2022-05-15 23:37:47");
+
+INSERT INTO users VALUE(default, 'SarahM', "Sarah", "Maouche", "saramaouchestv@gmail.com", "$2b$10$jvrFOpC.bpJvJrS6FfzCL.KGXGy18CN1LLltTQ4R5GwN0q3N9oC1i", "http://image.noelshack.com/fichiers/2022/20/2/1652755780-33967-214488522025027-467432520-n.jpeg", "2000/10/02", 2, 1, default, default, "2022-05-15 23:37:47", "2022-05-15 23:37:47");
+
+INSERT INTO users VALUE(default, 'JeSuisBan', "Bani", "Bani", "banban@gmail.com", "$2b$10$jvrFOpC.bpJvJrS6FfzCL.KGXGy18CN1LLltTQ4R5GwN0q3N9oC1i", default, "2000/01/01", default, 2, default, default, "2022-05-15 23:37:47", "2022-05-15 23:37:47");
+
+INSERT INTO users VALUE(default, 'racisteDu93', "Éric", "Zemmour", "eric.zemmou@gmail.com", "$2b$10$jvrFOpC.bpJvJrS6FfzCL.KGXGy18CN1LLltTQ4R5GwN0q3N9oC1i", "https://information.tv5monde.com/sites/info.tv5monde.com/files/styles/large/public/941b4fd760e622d66af9d61d483baf5b169254cd.jpg?itok=BtX7k2Na", "31/08/31", 1, 1, default, default, "2022-05-15 23:37:47", "2022-05-15 23:37:47");
+
+INSERT INTO users VALUE(default, 'jeSuisNezCommeCa', "Jean", "Lassalle", "jean.lassalle@gmail.com", "$2b$10$jvrFOpC.bpJvJrS6FfzCL.KGXGy18CN1LLltTQ4R5GwN0q3N9oC1i", "https://www.gala.fr/imgre/fit/http.3A.2F.2Fprd2-bone-image.2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2Fprismamedia_people.2F2017.2F06.2F30.2Faed3f014-f256-484c-b3f8-0cc2a18d9c4c.2Ejpeg/1285x891/quality/80/jean-lassalle.jpeg", "1955/05/03", 1, 1, default, default, "2022-05-15 23:37:47", "2022-05-15 23:37:47");
